1c8ba56ff643cde49ca5e335dc15b4c2fd10d321
PABLO-LAPTOP\pablo <pabloandresboada@gmail.com>
2018-03-11 23:10:27 -0500

    envio de correo por SES en views

--- concursos/settings.py
+++ concursos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['V_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ['V_HOST_EC2_AMAZON']
+ALLOWED_HOSTS = [os.environ['V_HOST_EC2_AMAZON']]
 
 
 # Application definition
